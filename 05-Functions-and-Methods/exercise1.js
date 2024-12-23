72bc50a394a1c97d4356e56afd3fa8bfb479e9fc
Manas kolaskar <manas.kolaskar@somaiya.edu>
2024-12-23 13:31:53 +0000

    Functions and Methods

---
+++ 05-Functions-and-Methods/exercise1.js
@@ -0,0 +1,23 @@
+// create a function using function keyword that takes string as an argument and returns the number of vowels in the string
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// create an arrow function to do dame task
+const countVowelsArrow = (str) => {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+};
---
+++ 05-Functions-and-Methods/exercise2.js
@@ -0,0 +1,6 @@
+// for a given array of numbers print the square of each value using for each loop
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(number => {
+    console.log(number * number);
+});
